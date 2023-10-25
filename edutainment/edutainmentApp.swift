4784d73799c656faaec8dab2402559aae03b8766
aydin :^) <a219201085@unison.mx>
2023-10-25 11:05:10 -0700

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by aydin salman on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
